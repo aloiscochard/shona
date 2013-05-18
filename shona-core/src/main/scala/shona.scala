441f32d4943c4a27feed008dca3180e634bc676d
Alois Cochard <alois.cochard@gmail.com>
2013-05-18 18:10:45 +0100

    Add value dependent type support (using string singleton type).
    - A copy of @milessabin implementation in shapeless/topic/paradise

---
+++ shona-core/src/main/scala/shona.scala
@@ -0,0 +1,35 @@
+import language.experimental.macros
+import scala.reflect.macros.{Context, Macro}
+
+// Credits to @milesabin for the SingeltonType implementation -> https://github.com/milessabin/shapeless/tree/topic/macro-paradise
+
+package shona {
+  case class Label[T](value: T)
+
+  trait SingletonTypeMacros extends Macro {
+    import c.universe._
+
+    def eval[T](t: c.Tree) = c.eval(c.Expr[T](c.resetAllAttrs(t.duplicate)))
+
+    def singletonType[T](t: c.Expr[T]) =
+      TypeTree(ConstantType(Constant(
+        eval[T](t.tree)
+      )))
+
+    def witnessLabel[T: c.WeakTypeTag] = {
+      weakTypeOf[T] match {
+        case t @ ConstantType(Constant(s)) =>
+          val T = TypeTree(t)
+          val l = Literal(Constant(s))
+          c.Expr[T](q"new Label[$T]($l)")
+        case _ => c.abort(c.enclosingPosition, "Type argument must be a singleton type")
+      }
+    }
+  }
+}
+
+package object shona {
+  type label[T](t: T) = macro SingletonTypeMacros.singletonType[T]
+  implicit def witnessLabel[T]: Label[T] = macro SingletonTypeMacros.witnessLabel[T]
+  def singleton[T](implicit label: Label[T]): T = label.value
+}
